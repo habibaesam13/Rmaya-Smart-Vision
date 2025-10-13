a86b286f9e6b191d680b86ff8d3ea4c3cc44886f
Habiba <habebaesam13@gmail.com>
2025-10-13 13:42:34 +0300

    hint before group reg form added

--- resources/views/PublicRegistration/GroupRegForm.blade.php
+++ resources/views/PublicRegistration/GroupRegForm.blade.php
@@ -83,6 +83,10 @@
 
 
         <div class="tabel-container ">
+          <div class="text-warning d-flex m-2 gap-4">
+            <p>الاسم يجب أن يكون باللغة العربية فقط</p>
+            <p>الهاتف : 055-  XXXXXXX</p>
+          </div>
           <table>
             <thead>
               <tr>
